90f1e44b4c14b8801dbacc4ace058b002ee9a6ab
ramyamounir <44840015+ramyamounir@users.noreply.github.com>
2025-03-11 14:51:13 -0400

    added obj error to stats

--- src/tbp/monty/frameworks/models/evidence_unsuperversed_matching.py
+++ src/tbp/monty/frameworks/models/evidence_unsuperversed_matching.py
@@ -27,95 +27,97 @@ from tbp.monty.frameworks.utils.logging_utils import maybe_rename_existing_file
 
 
 class MontyForUnsupervisedEvidenceGraphMatching(MontyForEvidenceGraphMatching):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.init_pre_episode = False
+    pass
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.init_pre_episode = False
 
-    def pre_episode(self, primary_target, semantic_id_to_label=None):
-        if not self.init_pre_episode:
-            self.init_pre_episode = True
-            return super().pre_episode(primary_target, semantic_id_to_label)
+    # def pre_episode(self, primary_target, semantic_id_to_label=None):
+    #     if not self.init_pre_episode:
+    #         self.init_pre_episode = True
+    #         return super().pre_episode(primary_target, semantic_id_to_label)
 
-        self.learning_modules[0].curr_ep += 1
-        self.min_eval_steps = 100
+    #     # self.learning_modules[0].curr_ep += 1
+    #     self.min_eval_steps = 100
 
-        self._is_done = False
-        self.reset_episode_steps()
-        self.switch_to_matching_step()
-        self.primary_target = primary_target
-        self.semantic_id_to_label = semantic_id_to_label
+    #     self._is_done = False
+    #     self.reset_episode_steps()
+    #     self.switch_to_matching_step()
+    #     self.primary_target = primary_target
+    #     self.semantic_id_to_label = semantic_id_to_label
 
-        for lm in self.learning_modules:
-            lm.primary_target = primary_target["object"]
-            lm.primary_target_rotation_quat = primary_target["quat_rotation"]
+    #     for lm in self.learning_modules:
+    #         lm.primary_target = primary_target["object"]
+    #         lm.primary_target_rotation_quat = primary_target["quat_rotation"]
 
 
 class UnuspervisedEvidenceGraphLM(EvidenceGraphLM):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.curr_ep = 1
-        self.updated_graph_ids = []
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.curr_ep = 1
+    #     self.updated_graph_ids = []
 
-    def _update_evidence(self, features, displacements, graph_id):
-        super()._update_evidence(features, displacements, graph_id)
+    # def _update_evidence(self, features, displacements, graph_id):
+    #     super()._update_evidence(features, displacements, graph_id)
 
-        if self.curr_ep == 2 and graph_id not in self.updated_graph_ids:
-            self.updated_graph_ids.append(graph_id)
+    #     if self.curr_ep == 2 and graph_id not in self.updated_graph_ids:
+    #         self.updated_graph_ids.append(graph_id)
 
-            (
-                initial_possible_channel_locations,
-                initial_possible_channel_rotations,
-                channel_evidence,
-            ) = self._get_initial_hypothesis_space(features, graph_id, "patch")
+    #         (
+    #             initial_possible_channel_locations,
+    #             initial_possible_channel_rotations,
+    #             channel_evidence,
+    #         ) = self._get_initial_hypothesis_space(features, graph_id, "patch")
 
-            self._add_hypotheses_to_hpspace(
-                graph_id=graph_id,
-                input_channel="patch",
-                new_loc_hypotheses=initial_possible_channel_locations,
-                new_rot_hypotheses=initial_possible_channel_rotations,
-                new_evidence=channel_evidence,
-            )
+    #         self._add_hypotheses_to_hpspace(
+    #             graph_id=graph_id,
+    #             input_channel="patch",
+    #             new_loc_hypotheses=initial_possible_channel_locations,
+    #             new_rot_hypotheses=initial_possible_channel_rotations,
+    #             new_evidence=channel_evidence,
+    #         )
 
-    def _add_hypotheses_to_hpspace(
-        self,
-        graph_id,
-        input_channel,
-        new_loc_hypotheses,
-        new_rot_hypotheses,
-        new_evidence,
-    ):
-        """Add new hypotheses to hypothesis space."""
-        # Add current mean evidence to give the new hypotheses a fighting
-        # chance. TODO H: Test mean vs. median here.
-        current_mean_evidence = np.mean(self.evidence[graph_id])
-        new_evidence = new_evidence + current_mean_evidence
-        # Add new hypotheses to hypothesis space
-        self.possible_locations[graph_id] = np.vstack(
-            [
-                self.possible_locations[graph_id],
-                new_loc_hypotheses,
-            ]
-        )
-        self.possible_poses[graph_id] = np.vstack(
-            [
-                self.possible_poses[graph_id],
-                new_rot_hypotheses,
-            ]
-        )
-        self.evidence[graph_id] = np.hstack([self.evidence[graph_id], new_evidence])
-        # Update channel hypothesis mapping
-        old_num_hypotheses = self.channel_hypothesis_mapping[graph_id]["num_hypotheses"]
-        new_num_hypotheses = old_num_hypotheses + len(new_loc_hypotheses)
-        self.channel_hypothesis_mapping[graph_id][input_channel] = [
-            0,
-            new_num_hypotheses,
-        ]
-        self.channel_hypothesis_mapping[graph_id]["num_hypotheses"] = new_num_hypotheses
+    # def _add_hypotheses_to_hpspace(
+    #     self,
+    #     graph_id,
+    #     input_channel,
+    #     new_loc_hypotheses,
+    #     new_rot_hypotheses,
+    #     new_evidence,
+    # ):
+    #     """Add new hypotheses to hypothesis space."""
+    #     # Add current mean evidence to give the new hypotheses a fighting
+    #     # chance. TODO H: Test mean vs. median here.
+    #     current_mean_evidence = np.mean(self.evidence[graph_id])
+    #     new_evidence = new_evidence + current_mean_evidence
+    #     # Add new hypotheses to hypothesis space
+    #     self.possible_locations[graph_id] = np.vstack(
+    #         [
+    #             self.possible_locations[graph_id],
+    #             new_loc_hypotheses,
+    #         ]
+    #     )
+    #     self.possible_poses[graph_id] = np.vstack(
+    #         [
+    #             self.possible_poses[graph_id],
+    #             new_rot_hypotheses,
+    #         ]
+    #     )
+    #     self.evidence[graph_id] = np.hstack([self.evidence[graph_id], new_evidence])
+    #     # Update channel hypothesis mapping
+    #     old_num_hypotheses = self.channel_hypothesis_mapping[graph_id]["num_hypotheses"]
+    #     new_num_hypotheses = old_num_hypotheses + len(new_loc_hypotheses)
+    #     self.channel_hypothesis_mapping[graph_id][input_channel] = [
+    #         0,
+    #         new_num_hypotheses,
+    #     ]
+    #     self.channel_hypothesis_mapping[graph_id]["num_hypotheses"] = new_num_hypotheses
 
     def _add_detailed_stats(self, stats, get_rotations):
         stats = super()._add_detailed_stats(stats, get_rotations)
         stats["th_limit"] = self._calculate_theoretical_limit()
         stats["mlh_error"] = self._get_pose_error_mlh()
+        stats["obj_error"] = self._get_pose_error_target()
         return stats
 
     def _calculate_theoretical_limit(self):
@@ -127,6 +129,16 @@ class UnuspervisedEvidenceGraphLM(EvidenceGraphLM):
     def _get_pose_error_mlh(self):
         target_rot = Rotation.from_quat(self.primary_target_rotation_quat)
         difference = self.current_mlh["rotation"] * target_rot.inv()
+        return difference.magnitude()
+
+    def _get_pose_error_target(self):
+        target_rot = Rotation.from_quat(self.primary_target_rotation_quat)
+        obj_mlh_id = np.argmax(self.evidence[self.primary_target])
+        obj_rotation = Rotation.from_matrix(
+            self.possible_poses[self.primary_target][obj_mlh_id]
+        )
+
+        difference = obj_rotation * target_rot.inv()
         return difference.magnitude()
 
     def get_pose_error(self, detected_pose, target_pose):
@@ -169,6 +181,7 @@ class MaxEvidenceJSONHandler(DetailedJSONHandler):
         lm_data["th_limit"] = data["DETAILED"][total]["LM_0"]["th_limit"]
         lm_data["current_mlh"] = data["DETAILED"][total]["LM_0"]["current_mlh"]
         lm_data["mlh_error"] = data["DETAILED"][total]["LM_0"]["mlh_error"]
+        lm_data["obj_error"] = data["DETAILED"][total]["LM_0"]["obj_error"]
 
         data["DETAILED"][total] = {"LM_0": {}}
         data["DETAILED"][total]["LM_0"] = lm_data
